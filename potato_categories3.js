14d020cba029dce5ce1c994af25035006cf39fbc
Colin Perepelken <colin@perepelken.ca>
2022-01-10 18:11:34 -0800

    Update jquery, bootstrap, and createjs sources

--- potato_categories3.js
+++ potato_categories3.js
@@ -1,7 +1,7 @@
 /**
- * BCLearningNetwork.com
+ * wcln.ca
  * Categories Quiz Game
- * @author Colin Bernard (colinjbernard@hotmail.com)
+ * @author Colin Perepelken (colin@perepelken.ca)
  * June 2017
  */
 
